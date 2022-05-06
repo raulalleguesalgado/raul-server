0945ad4ef5b952f31eb1dd083dba5435916eadc8
Raulas <raulalleguesalgado1@gmail.com>
2022-05-06 12:46:58 +0200

    first commit

---
+++ src/main/java/Entity/Collection.java
@@ -0,0 +1,31 @@
+package Entity;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="coleccion", uniqueConstraints = {@UniqueConstraint(name = "uq_coleccion_nombre", columnNames = "nombre")})
+
+
+public class Collection {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	 @NotBlank private String nombre;
+	 @NotBlank private String publicador;
+	 
+}
