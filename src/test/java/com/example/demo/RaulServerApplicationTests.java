c80cc14c238d66fa46ff60d4a7813c24cbefc582
Raulas <raulalleguesalgado1@gmail.com>
2022-06-15 23:05:14 +0200

    Updated test folder in order to export jar

--- src/test/java/com/example/demo/RaulServerApplicationTests.java
+++ src/test/java/com/example/demo/RaulServerApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+import com.raulallegue.app.RaulApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes={RaulApplication.class})
 class RaulServerApplicationTests {
 
 	@Test
